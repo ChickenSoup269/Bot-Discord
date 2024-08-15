865e23949aa0969b785f0d0909d28d6333cec473
TranThien <thientran01345@icloud.com>
2024-08-15 21:52:17 +0700

    V4 BotDiscord [BETA]

--- normal.py
+++
@@ -1,9 +0,0 @@
-def is_leap_year(year):
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        print(year , 'là năm nhuận') 
-    else:
-        print(year , ' không phải là năm nhuận') 
-
-
-year = int(input('Nhập năm bất kỳ: '))
-is_leap_year(year)
